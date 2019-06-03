51b89c611b51d23d64696033affc3c4b14d27cb9
nilosimpson <51337938+nilosimpson@users.noreply.github.com>
2019-06-03 14:58:26 -0400

    Create data_lixe_tests.py

---
+++ data_lixe_tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestDataLixe(unittest.TestCase):
+
+    def test_true(self):
+        self.assertEqual(True, True, "Should be True")
+
+if __name__ == '__main__':
+    unittest.main()
