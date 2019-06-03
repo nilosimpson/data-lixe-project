8d0adb67354e962ebf2e78cceb93a623258b5161
nilosimpson <51337938+nilosimpson@users.noreply.github.com>
2019-06-03 15:06:00 -0400

    Added test_false test

--- data_lixe_tests.py
+++ data_lixe_tests.py
@@ -5,5 +5,8 @@ class TestDataLixe(unittest.TestCase):
     def test_true(self):
         self.assertEqual(True, True, "Should be True")
 
+    def test_false(self):
+        self.assertEqual(False, False, "Should be True")
+
 if __name__ == '__main__':
     unittest.main()
